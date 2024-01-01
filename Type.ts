b8a74444c2e03838b61d90e0b79bcebfb53eead2
Apiwatz Lee <apiwatz.lee@gmail.com>
2024-01-01 12:42:09 +0700

    type annotations

--- Type.ts
+++ Type.ts
@@ -2,3 +2,6 @@
 
 //Primitive Types : Number,Boolean,Void,Undefined,String,Symbol,Null
 //Object Types: function,arrays,classes,objects
+
+//Type annotations: Code we add to tell Typescript what type of value a variable will refer to
+//Type inference: Typescript tries to figure out what type of value a variable refers to
---
+++ annotations/variables.ts
@@ -0,0 +1,11 @@
+const apple: number = 5;
+const speed: string = 'fast';
+const hasName: boolean = true;
+const nothingMuch: null = null;
+const nothing: undefined = undefined;
+
+//built in objects
+let now: Date = new Date();
+
+//Array
+let colors: string[] = ['red', 'green', 'blue'];

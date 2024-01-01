ec517bbe49d7df720886404590b1a727404a0b2c
Apiwatz Lee <apiwatz.lee@gmail.com>
2024-01-01 12:16:02 +0700

    2 types

---
+++ Type.js
@@ -0,0 +1,4 @@
+//Type --> Easy way to refer the different properties + function that value has
+
+//Primitive Types : Number,Boolean,Void,Undefined,String,Symbol,Null
+//Object Types: function,arrays,classes,objects

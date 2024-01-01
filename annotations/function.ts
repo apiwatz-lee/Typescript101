ecf69d11499e51b87d6a5361b938d4d7e3089d21
Apiwatz Lee <apiwatz.lee@gmail.com>
2024-01-01 23:52:37 +0700

    feat:variable annotation

---
+++ annotations/function.ts
@@ -0,0 +1,6 @@
+//Type annotation : Code we add to tell Typescript what type of arguments a function will receive and what type of values it will return
+//Type inference : Typescript tries to figure out what type of value a function will return
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
